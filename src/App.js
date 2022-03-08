9040f521ff9d7a0858aa63917a44d293f7e7c054
Yash Sharma <86138890+Yashkr00129@users.noreply.github.com>
2022-03-09 00:14:02 +0530

    UI Complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Login from "./Components/Views/Auth/Login";
 import Register from "./Components/Views/Auth/Register";
+import Cart from "./Components/Views/Cart/Cart";
 import Homepage from "./Components/Views/Homepage/Homepage";
 import Product from "./Components/Views/Product/Product";
 import ProductList from "./Components/Views/ProductList/ProductList";
 
 function App() {
-  return <Login />;
+  return <Cart />;
 }
 
 export default App;
